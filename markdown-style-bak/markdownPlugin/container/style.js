b6e671466a7830549ba33cc9da0ca1170c2104a5
rainboy <rainboylvx@qq.com>
2025-10-05 08:47:05 +0800

    del

--- markdown-style-bak/markdownPlugin/container/style.js
+++
@@ -1,9 +0,0 @@
-//利用markdown it container 做一个可以直接写 style的 container
-//::: style background-color:red; color:blue;
-//...
-//:::
-//
-//渲染成---> 
-//<div style="background-color:red; color:blue;">
-//...
-//</div>
--- markdown-style-bak/markdownPlugin/readme.md
+++
@@ -1,2 +0,0 @@
-pseudocode,一个可以在网页上渲染伪代码的库,https://github.com/SaswatPadhi/pseudocode.js
-可以在它的github主页上查看对应的语法
