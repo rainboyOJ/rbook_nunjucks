cc2999121066c1a5018fc2df7b9d7a9c39e34850
Rainboylvx <Rainboylvx@users.noreply.github.com>
2025-11-27 11:21:12 +0000

    auto deploy 8cb6639b01a5204fa2e3a2aaf1752f8627c4d1e2

--- code_template/code/base/binary_search.cpp
+++ code_template/code/base/binary_search.cpp
@@ -8,6 +8,9 @@ bool check(int pos,int val){
 
 //bs_find = binary search find
 // 返回第一个满足条件的位置
+// 如果所有的值都不满足条件,返回r, 
+// !!! r位置对应的是Guard,一个虚拟的位置,保证一定各满足check
+// !!! 写题目的时候一定要注意r的值,通常是n+1
 // 保障初始区间[l,r],r一定是满足的,通常r = n+1
 int bs_find(int l,int r,int val) {
     while( l < r) {
